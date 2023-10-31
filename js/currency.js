de79da854b4efbe6b9ee9ab966cbe702fa54ff04
Wojtek Dziugiel <wojtek.dziugiel@gmail.com>
2023-10-31 05:27:15 +0100

    code errors corrected

--- js/currency.js
+++ js/currency.js
@@ -1,37 +1,41 @@
 {
-  const calculateResult = (value, currency) => {
+  const calculateResult = (amount, currency) => {
     const EUR = 4.59;
     const USD = 4.16;
     const HRK = 0.6;
 
     switch (currency) {
       case "EUR":
-        return value / EUR;
+        return amount / EUR;
 
       case "USD":
-        return value / USD;
+        return amount / USD;
 
       case "HRK":
-        return value / HRK;
+        return amount / HRK;
     }
   };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const amountElement = document.querySelector(".js-amount");
+    const currencyResult = document.querySelector(".js-result");
+    const currencyOption = document.querySelector(".js-currency");
+
+    const currency = currencyOption.value;
+    const amount = amountElement.value;
+
+    const result = calculateResult(amount, currency);
+
+    currencyResult.innerHTML = `${result.toFixed(2)} ${currency}<strong>`;
+  };
+
   const init = () => {
     const fieldsetElement = document.querySelector(".js-form");
 
-    fieldsetElement.addEventListener("submit", (event) => {
-      event.preventDefault();
-
-      const valueElement = document.querySelector(".js-value");
-      const resultElement = document.querySelector(".js-result");
-      const currencyElement = document.querySelector(".js-currency");
-
-      const currency = currencyElement.value;
-      const value = valueElement.value;
-
-      const result = calculateResult(value, currency);
-
-      resultElement.innerHTML = `${result.toFixed(2)} ${currency}<strong>`;
-    });
+    fieldsetElement.addEventListener("submit", onFormSubmit);
   };
+
   init();
 }
